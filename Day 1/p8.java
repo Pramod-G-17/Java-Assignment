670021d15d032bdb3d69277697120fa648922a6b
atharv ghadge <atharvghadge09@gmail.com>
2024-09-30 19:46:52 +0530

    Day 1 Added

---
+++ Day 1/p8.java
@@ -0,0 +1,18 @@
+class pamu{
+	public static void main(String[]args){
+		for(int a=15;a>=0;a--){
+			if(a>12){
+				System.out.print(a+" ");
+			}else if(a==0){
+					System.out.print(a+" ");
+			}else{
+				System.out.print("."+" ");
+			}
+		}
+		System.out.println();
+	}
+}
+
+O/P:-
+
+15 14 13 . . . . . . . . . . . . 0
