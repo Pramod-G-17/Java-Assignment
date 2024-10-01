b41d735942c631f3fa5f5444c79f1d965260dc69
atharv ghadge <atharvghadge09@gmail.com>
2024-10-01 17:43:54 +0530

    Day 2 Added

---
+++ Day 2/p4.java
@@ -0,0 +1,14 @@
+class pamu{
+	public static void main(String[]args){
+		for(int a=1;a<=20;a++){
+			if(a%3==0 && a%7==0){
+				System.out.print(a+" ");
+			}else{
+				System.out.print(" ");
+			}
+		}	
+	}
+}
+
+O/P:-
+
---
+++ Day 2/p6.java
@@ -0,0 +1,12 @@
+class pamu{
+	public static void main(String[]args){
+		int a=1;
+		while(a%3==0 && a%7==0){
+			System.out.print(a+" ");
+			a++;
+		}
+	}
+}
+
+O/P:-
+
